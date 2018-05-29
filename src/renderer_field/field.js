dbe9138cc16d2b731d1579e09230289f2b2cf1fc
takty <tokutadaginaya@gmail.com>
2018-05-29 19:03:37 +0900

    Refactoring

--- src/renderer_field/field.js
+++ src/renderer_field/field.js
@@ -8,54 +8,55 @@
  */
 
 
-{
-	'use strict';
+'use strict';
+
+
+window.addEventListener('DOMContentLoaded', () => { new Field(); });
+
+
+class Field {
 	
-	class Field {
-		
-		constructor(containerSel) {
-			this._id = window.location.hash.replace('#', '');
-			this._container = document.querySelector(containerSel);
+	constructor() {
+		this._id = window.location.hash.replace('#', '');
+		this._container = document.createElement('div');
+		document.body.appendChild(this._container);
 
-			window.ondragover = window.ondrop = (e) => {e.preventDefault(); return false;};
+		window.ondragover = window.ondrop = (e) => {e.preventDefault(); return false;};
 
-			window.addEventListener('storage', (e) => {
-				if ('field_' + this._id !== e.key) return;
-				window.localStorage.clear();
-				const ma = JSON.parse(e.newValue);
-				if (ma.message === 'callFieldMethod' && this[ma.params.method]) {
-					this[ma.params.method](...ma.params.args);
-				}
-			});
-		}
-
-		openProgram(url) {
-			this.closeProgram();
-
-			this._frame = document.createElement('iframe');
-			this._frame.setAttribute('src', url);
-			this._container.appendChild(this._frame);
-		}
-
-		closeProgram() {
-			if (!this._frame) return;
-			try {
-				this._container.removeChild(this._frame);
-			} catch (e) { }
-			this._frame = null;
-		}
-
-		onWindowFullscreenEntered() {
-			if (!this._frame) return;
-			window.localStorage.setItem('injection_' + this._id, JSON.stringify({ message: 'window-fullscreen-entered' }));
-		}
-
-		onWindowFullscreenLeft() {
-			if (!this._frame) return;
-			window.localStorage.setItem('injection_' + this._id, JSON.stringify({ message: 'window-fullscreen-left' }));
-		}
-	
+		window.addEventListener('storage', (e) => {
+			if ('field_' + this._id !== e.key) return;
+			window.localStorage.clear();
+			const ma = JSON.parse(e.newValue);
+			if (ma.message === 'callFieldMethod' && this[ma.params.method]) {
+				this[ma.params.method](...ma.params.args);
+			}
+		});
 	}
 
-	global.Field = Field;
+	openProgram(url) {
+		this.closeProgram();
+
+		this._frame = document.createElement('iframe');
+		this._frame.setAttribute('src', url);
+		this._container.appendChild(this._frame);
+	}
+
+	closeProgram() {
+		if (!this._frame) return;
+		try {
+			this._container.removeChild(this._frame);
+		} catch (e) { }
+		this._frame = null;
+	}
+
+	onWindowFullscreenEntered() {
+		if (!this._frame) return;
+		window.localStorage.setItem('injection_' + this._id, JSON.stringify({ message: 'window-fullscreen-entered' }));
+	}
+
+	onWindowFullscreenLeft() {
+		if (!this._frame) return;
+		window.localStorage.setItem('injection_' + this._id, JSON.stringify({ message: 'window-fullscreen-left' }));
+	}
+
 }
