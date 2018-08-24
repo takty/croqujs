0516ebdef71a4e10370e9e46edd3715a5079b34b
Takty <tokutadaginaya@gmail.com>
2018-08-24 10:34:01 +0900

    Refactoring package.json and gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,87 +1,81 @@
 'use strict';
 
 const fs = require('fs-extra');
+const glob = require('glob');
 const path = require('path');
 const gulp = require('gulp');
 const $ = require('gulp-load-plugins')({pattern:['gulp-*']});
 
-gulp.task('copy-tern', (done) => {
-	fs.copySync('./node_modules/tern/lib', './dist/renderer_study/lib/tern');
-	fs.copySync('./node_modules/tern/defs', './dist/renderer_study/lib/tern');
-	done();
-});
+function copySync(from, to) {
+	const isToDir = to.endsWith('/');
+	const files = glob.sync(from);
+	for (let f of files) {
+		if (isToDir) {
+			const fn = path.basename(f);
+			fs.copySync(f, path.join(to, fn));
+		} else {
+			fs.copySync(f, to);
+		}
+	}
+}
 
-gulp.task('copy-sweetalert2', (done) => {
-	fs.copySync('./node_modules/sweetalert2/dist/sweetalert2.min.*', './dist/renderer_study/lib/sweetalert2');
-	done();
-});
-
-gulp.task('copy-js-beautify', (done) => {
-	fs.copySync('./node_modules/js-beautify/js/lib/beautify.js', './dist/renderer_study/lib/js-beautify');
-	done();
-});
-
-gulp.task('copy-font-awesome', (done) => {
-	fs.copySync('./node_modules/font-awesome/css/font-awesome.min.css', './dist/renderer_study/lib/font-awesome/css');
-	fs.copySync('./node_modules/font-awesome/fonts/fontawesome-webfont.woff2', './dist/renderer_study/lib/font-awesome/fonts');
-	done();
-});
+const PATH_STUDY_LIB = './dist/renderer_study/lib/';
 
 gulp.task('copy-acorn', (done) => {
-	fs.copySync('./node_modules/acorn/dist', './dist/renderer_study/lib/acorn');
-	fs.copySync('./node_modules/acorn/dist', './dist/lib/acorn');
+	copySync('./node_modules/acorn/dist', PATH_STUDY_LIB + 'acorn');
+	copySync('./node_modules/acorn/dist', './dist/lib/acorn');
 	done();
 });
 
 gulp.task('copy-codemirror', (done) => {
-	fs.copySync('./node_modules/codemirror/lib', './dist/renderer_study/lib/codemirror/lib');
-	fs.copySync('./node_modules/codemirror/addon', './dist/renderer_study/lib/codemirror/addon');
-	fs.copySync('./node_modules/codemirror/mode/javascript', './dist/renderer_study/lib/codemirror/mode/javascript');
+	copySync('./node_modules/codemirror/lib', PATH_STUDY_LIB + 'codemirror/lib');
+	copySync('./node_modules/codemirror/addon', PATH_STUDY_LIB + 'codemirror/addon');
+	copySync('./node_modules/codemirror/mode/javascript', PATH_STUDY_LIB + 'codemirror/mode/javascript');
 	done();
 });
 
-gulp.task('copy', gulp.parallel(
-	'copy-tern',
-	'copy-sweetalert2',
-	'copy-js-beautify',
-	'copy-font-awesome',
+gulp.task('copy-font-awesome', (done) => {
+	copySync('./node_modules/font-awesome/css/font-awesome.min.css', PATH_STUDY_LIB + 'font-awesome/css/');
+	copySync('./node_modules/font-awesome/fonts/fontawesome-webfont.woff2', PATH_STUDY_LIB + 'font-awesome/fonts/');
+	done();
+});
+
+gulp.task('copy-js-beautify', (done) => {
+	copySync('./node_modules/js-beautify/js/lib/beautify.js', PATH_STUDY_LIB + 'js-beautify/');
+	done();
+});
+
+gulp.task('copy-sweetalert2', (done) => {
+	copySync('./node_modules/sweetalert2/dist/sweetalert2.min.*', PATH_STUDY_LIB + 'sweetalert2/');
+	done();
+});
+
+gulp.task('copy-tern', (done) => {
+	copySync('./node_modules/tern/lib', PATH_STUDY_LIB + 'tern');
+	copySync('./node_modules/tern/defs', PATH_STUDY_LIB + 'tern');
+	done();
+});
+
+gulp.task('copy-lib', gulp.parallel(
 	'copy-acorn',
-	'copy-codemirror'
+	'copy-codemirror',
+	'copy-font-awesome',
+	'copy-js-beautify',
+	'copy-sweetalert2',
+	'copy-tern',
 ));
 
-// gulp.task('js-with-option', () => {
-// 	return gulp.src('src/js/**/*.js')
-// 		.pipe($.plumber())
-// 		.pipe($.babel({presets: [['env', {targets: {ie: 11}}]]}))
-// 		.pipe($.concat('stile-full.min.js'))
-// 		.pipe($.uglify())
-// 		.pipe(gulp.dest('dist/js'));
-// });
+gulp.task('copy-src', (done) => {
+	copySync('./src', './dist');
+	fs.removeSync('./dist/renderer_study/scss/');
+	done();
+});
 
-// gulp.task('js-without-option', () => {
-// 	return gulp.src(['src/js/basic/*.js', 'src/js/content/*.js'])
-// 		.pipe($.plumber())
-// 		.pipe($.babel({presets: [['env', {targets: {ie: 11}}]]}))
-// 		.pipe($.concat('stile.min.js'))
-// 		.pipe($.uglify())
-// 		.pipe(gulp.dest('dist/js'));
-// });
-
-// gulp.task('js-each', () => {
-// 	return gulp.src('src/js/**/*.js')
-// 		.pipe($.plumber())
-// 		.pipe($.babel({presets: [['env', {targets: {ie: 11}}]]}))
-// 		.pipe($.uglify())
-// 		.pipe($.rename({extname: '.min.js'}))
-// 		.pipe(gulp.dest('dist/js'));
-// });
-
-// gulp.task('js', gulp.parallel('js-with-option', 'js-without-option', 'js-each'));
+gulp.task('copy', gulp.series('copy-src', 'copy-lib'));
 
 gulp.task('sass', () => {
 	return gulp.src(['src/**/scss/**/[^_]*.scss'])
 		.pipe($.plumber())
-		// .pipe($.sourcemaps.init())
 		.pipe($.sass({outputStyle: 'compressed'}))
 		.pipe($.autoprefixer({browsers: ['ie >= 11'], remove: false}))
 		.pipe($.rename({extname: '.min.css'}))
@@ -89,7 +83,6 @@ gulp.task('sass', () => {
 			p.dirname = p.dirname.replace(path.sep + 'scss' + path.sep, path.sep + 'css' + path.sep);
 			p.dirname = p.dirname.replace(path.sep + 'scss', path.sep + 'css');
 		}))
-		// .pipe($.sourcemaps.write('.'))
 		.pipe(gulp.dest('dist'));
 });
 
@@ -103,44 +96,4 @@ gulp.task('sass-misc', () => {
 		.pipe(gulp.dest('dist'));
 });
 
-// gulp.task('watch', () => {
-// 	gulp.watch('src/js/**/*.js', gulp.series('js'));
-// 	gulp.watch('src/sass/**/*.scss', gulp.series('sass'));
-// });
-
-// gulp.task('build', gulp.parallel('js', 'sass'));
-
-// gulp.task('default', gulp.series('build', 'watch'));
-gulp.task('default', gulp.series('sass', 'sass-misc'));
-
-
-// -----------------------------------------------------------------------------
-
-// gulp.task('docs-sass', gulp.series('sass', () => {
-// 	return gulp.src('docs/style.scss')
-// 		.pipe($.plumber())
-// 		.pipe($.sourcemaps.init())
-// 		.pipe($.sass({outputStyle: 'compressed'}))
-// 		.pipe($.autoprefixer({browsers: ['ie >= 11'], remove: false}))
-// 		.pipe($.rename({extname: '.min.css'}))
-// 		.pipe($.sourcemaps.write('.'))
-// 		.pipe(gulp.dest('docs'));
-// }));
-
-// gulp.task('docs-js', gulp.series('js-with-option', () => {
-// 	return gulp.src(['dist/js/stile-full.min.js'])
-// 		.pipe($.plumber())
-// 		.pipe(gulp.dest('docs'));
-// }));
-
-// gulp.task('docs-watch', () => {
-// 	gulp.watch('src/js/**/*.js',     gulp.series('docs-js'));
-// 	gulp.watch('src/sass/**/*.scss', gulp.series('docs-sass'));
-// 	gulp.watch('docs/style.scss',    gulp.series('docs-sass'));
-// });
-
-// gulp.task('docs-build', gulp.parallel('docs-js', 'docs-sass'));
-
-// gulp.task('docs-default', gulp.series('docs-build', 'docs-watch'));
-
-// gulp.task('docs', gulp.parallel('default', 'docs-default'));
+gulp.task('default', gulp.series('copy', 'sass', 'sass-misc'));
