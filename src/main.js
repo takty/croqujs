6a1db5f3b57510d1d5efd1fb23029c445397acee
Takuto Yanagida <tokutadaginaya@gmail.com>
2018-11-27 11:30:16 +0900

    Remove native menus

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
  * Main (JS)
  *
  * @author Takuto Yanagida @ Space-Time Inc.
- * @version 2018-11-26
+ * @version 2018-11-27
  *
  */
 
@@ -29,21 +29,22 @@ class Main {
 		this._twins = [];
 		this._focusedTwin = null;
 
+		app.setName('Croqujs');  // for Mac
 		app.on('ready', () => { this._createNewWindow(); });
 		app.on('activate', () => { if (this._twins.length === 0) this._createNewWindow(); });
 		app.on('window-all-closed', () => {
 			this._conf.saveSync();
-			if (IS_MAC) Menu.setApplicationMenu(this._createNav());
-			else app.quit();
+			app.quit();
 		});
 		app.on('browser-window-focus', (ev, win) => {
 			if (IS_MAC && this._twins.length === 0) {
-				Menu.setApplicationMenu(this._createNav());
+				// Menu.setApplicationMenu(this._createNav());
 			} else {
 				this._focusedTwin = this._twins.find(t => t.isOwnerOf(win));
-				if (IS_MAC) Menu.setApplicationMenu(this._focusedTwin.menu());
+				// if (IS_MAC) Menu.setApplicationMenu(this._focusedTwin.menu());
 			}
 		});
+		// if (IS_MAC) Menu.setApplicationMenu(this._createNav());
 	}
 
 	_createNewWindow() {
@@ -54,6 +55,7 @@ class Main {
 	_createNav() {
 		return Menu.buildFromTemplate([{ label: 'File', submenu: [
 			{ label: 'New Window', accelerator: 'CmdOrCtrl+Shift+N',   click: this._createNewWindow.bind(this) },
+			{ label: 'Quit',       accelerator: 'CmdOrCtrl+Q',         role: 'quit' },
 			{ label: '',           accelerator: 'CmdOrCtrl+F12',       click: () => { this._focusedTwin['toggleFieldDevTools'](); }, visible: false },
 			{ label: '',           accelerator: 'CmdOrCtrl+Shift+F12', click: () => { this._focusedTwin['toggleStudyDevTools'](); }, visible: false },
 		] }]);
